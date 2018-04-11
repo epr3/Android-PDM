ac29f4e6e25935984df6fa9cb126ec4b42a6bb62
Eduard Predescu <eduard.florin.predescu@gmail.com>
2018-04-11 20:51:56 +0300

    finished google maps

--- app/src/main/java/com/ase/eu/android_pdm/TraseuAdapter.java
+++ app/src/main/java/com/ase/eu/android_pdm/TraseuAdapter.java
@@ -1,11 +1,14 @@
 package com.ase.eu.android_pdm;
 
+import android.content.Intent;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
+import java.io.Serializable;
 import java.util.List;
 
 class TraseuAdapter extends RecyclerView.Adapter<TraseuAdapter.ViewHolder> {
@@ -17,12 +20,14 @@ class TraseuAdapter extends RecyclerView.Adapter<TraseuAdapter.ViewHolder> {
 
     public static class ViewHolder extends RecyclerView.ViewHolder {
         public TextView denumire, dataStart, dataFinal, distanta;
+        public LinearLayout layout;
         public ViewHolder(final View v) {
             super(v);
             denumire = v.findViewById(R.id.denumire);
             dataStart = v.findViewById(R.id.dataStart);
             dataFinal = v.findViewById(R.id.dataFinal);
             distanta = v.findViewById(R.id.distanta);
+            layout = v.findViewById(R.id.recycler_view_row);
         }
     }
 
@@ -35,7 +40,7 @@ class TraseuAdapter extends RecyclerView.Adapter<TraseuAdapter.ViewHolder> {
     }
 
     @Override
-    public void onBindViewHolder(TraseuAdapter.ViewHolder holder, int position) {
+    public void onBindViewHolder(TraseuAdapter.ViewHolder holder, final int position) {
         TraseuPuncte traseu = lista.get(position);
         holder.denumire.setText(traseu.getTraseu().getDenumire());
         holder.dataFinal.setText(traseu.getTraseu().getDataFinal().toString());
@@ -43,6 +48,16 @@ class TraseuAdapter extends RecyclerView.Adapter<TraseuAdapter.ViewHolder> {
         holder.distanta.setText((traseu.getListaPuncte() != null
                 ? traseu.getListaPuncte().size()
                 : 0) + " puncte");
+        holder.layout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                TraseuPuncte traseu = lista.get(position);
+                Intent intent = new Intent(v.getContext(), MapsActivity.class);
+                intent.putExtra("listaPuncte", (Serializable) traseu.getListaPuncte());
+                v.getContext().startActivity(intent);
+            }
+        });
+
     }
 
     @Override
